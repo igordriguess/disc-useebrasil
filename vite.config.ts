50acbabc42f891566fd05286ec9e8078bc5351d4
Igor do Val Rodrigues Pinhanelli <irodrigues@ccmrp.local>
2026-04-10 00:31:44 -0300

    fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
+  base: "./",
+
   server: {
     host: "::",
     port: 8080,
@@ -16,11 +18,20 @@ export default defineConfig(() => ({
       overlay: false,
     },
   },
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+    dedupe: [
+      "react",
+      "react-dom",
+      "react/jsx-runtime",
+      "react/jsx-dev-runtime",
+      "@tanstack/react-query",
+      "@tanstack/query-core",
+    ],
   },
 }));
